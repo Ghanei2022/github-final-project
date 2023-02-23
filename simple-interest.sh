c6dd0c0caf91ef74a881882c0d172d32ecc6ef2e
Ghanei2022 <119994975+Ghanei2022@users.noreply.github.com>
2023-02-23 17:46:02 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,28 +1,9 @@
-#!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+const http = require('http');
 
-   # Do not use this in production. Sample purpose only.
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/string'});
+   let dt = new Date();
+   response.end("This is a new string returned on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
+}).listen(8080);
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <Ghanei2022>
-
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
-
-   # Output:
-   # simple interest = p*t*r
-
-   echo "Enter the principal:"
-   read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
-
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+console.log('Server running at http://0.0.0.0:8080/');
